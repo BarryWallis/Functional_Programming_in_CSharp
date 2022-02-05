95e2fa9c45d4f93577df0bc48c794e427ed5d2bb
Barry Wallis <barry.wallis@outlook.com>
2022-02-04 22:58:26 -0800

    Add project files.

---
+++ ExerciseSolutions/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ExerciseSolutions.Chapter02;
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
---
+++ ExerciseTests/Chapter02/Chapter02Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static ExerciseSolutions.Chapter02.Chapter02Exercises;
+
+namespace ExerciseTests.Chapter02;
+
+[TestClass]
+public class Chapter02Tests
+{
+    [TestMethod]
+    [DataRow(4, false)]
+    [DataRow(5, true)]
+    public void MyTestMethod(int arg, bool expectedResult)
+    {
+        Func<int, bool> isMod2 = (int i) => i % 2 == 0;
+        Func<int, bool>? negateIsMode2 = isMod2.Negate();
+        bool actualResult = negateIsMode2(arg);
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
