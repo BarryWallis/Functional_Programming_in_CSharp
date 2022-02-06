b42c8ce8f374690306e8d57d0c2971e2b68f6492
Barry Wallis <barry.wallis@outlook.com>
2022-02-05 22:56:37 -0800

    Chapter 2 exercises.

--- ExerciseSolutions/Chapter02/Chapter02Exercises.cs
+++ ExerciseSolutions/Chapter02/Chapter02Exercises.cs
@@ -4,23 +4,73 @@ using System.Linq;
 
 namespace ExerciseSolutions.Chapter02;
 
- public static class Chapter02Exercises
+public static class Chapter02Exercises
 {
-    // 1. Write a function that negates a given predicate: whenvever the given predicate
-    // evaluates to `true`, the resulting function evaluates to `false`, and vice versa.
+    /// <summary>
+    /// 1. Write a function that negates a given predicate: whenvever the given predicate
+    /// evaluates to `true`, the resulting function evaluates to `false`, and vice versa.
+    /// </summary>
+    /// <typeparam name="T">The type of the item to evaluate.</typeparam>
+    /// <param name="predicate">The predicate function to use to evaluate the item.</param>
+    /// <returns>The logical negation of applying the predicte.</returns>
     public static Func<T, bool> Negate<T>(this Func<T, bool> predicate) => t => !predicate(t);
 
-    // 2. Write a method that uses quicksort to sort a `List<int>` (return a new list,
-    // rather than sorting it in place).
+    /// <summary>
+    /// 2. Write a method that uses quicksort to sort a `List<int>` and returns a new list,
+    /// rather than sorting it in place).
+    /// </summary>
+    /// <param name="list">The list to sort.</param>
+    /// <returns>The new sorted list.</returns>
+    public static List<int> QuickSort(this List<int> list)
+    {
+        List<int> result;
+        if (list.Count == 0)
+        {
+            result = new();
+        }
+        else
+        {
+            int pivot = list[0];
+            IEnumerable<int> rest = list.Skip(1);
+            List<int> left = rest.Where(t => t.CompareTo(pivot) <= 0).ToList();
+            List<int> right = rest.Where(t => t.CompareTo(pivot) > 0).ToList();
+            result = QuickSort(left).Append(pivot)
+                                    .Concat(right.QuickSort())
+                                    .ToList();
+        }
 
+        return result;
+    }
 
     // 3. Generalize your implementation to take a `List<T>`, and additionally a 
     // `Comparison<T>` delegate.
+    /// <summary>
+    /// 3. Write a method that uses quicksort to sort a generic `List<T>` and returns a new list,
+    /// rather than sorting it in place.
+    /// </summary>
+    /// <typeparam name="T">The type of objects in the list.</typeparam>
+    /// <param name="list">The list to sort."</param>
+    /// <param name="compare">The function used to compare the objcts in the list.</param>
+    /// <returns></returns>
+    public static List<T> QuickSort<T>(this List<T> list, Comparison<T> compare)
+    {
 
-    // 4. In this chapter, you've seen a `Using` function that takes an `IDisposable`
-    // and a function of type `Func<TDisp, R>`. Write an overload of `Using` that
-    // takes a `Func<IDisposable>` as first
-    // parameter, instead of the `IDisposable`. (This can be used to fix warnings
-    // given by some code analysis tools about instantiating an `IDisposable` and
-    // not disposing it.)
+        List<T> result;
+        if (list.Count == 0)
+        {
+            result = new();
+        }
+        else
+        {
+            T pivot = list[0];
+            IEnumerable<T> rest = list.Skip(1);
+            List<T> left = rest.Where(t => compare(t, pivot) <= 0).ToList();
+            List<T> right = rest.Where(t => compare(t, pivot) > 0).ToList();
+            result = QuickSort(left, compare).Append(pivot)
+                                    .Concat(right.QuickSort(compare))
+                                    .ToList();
+        }
+
+        return result;
+    }
 }
--- ExerciseTests/Chapter02/Chapter02Tests.cs
+++ ExerciseTests/Chapter02/Chapter02Tests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 using static ExerciseSolutions.Chapter02.Chapter02Exercises;
 
 namespace ExerciseTests.Chapter02;
@@ -15,11 +17,29 @@ public class Chapter02Tests
     [TestMethod]
     [DataRow(4, false)]
     [DataRow(5, true)]
-    public void MyTestMethod(int arg, bool expectedResult)
+    public void Negate_Success(int arg, bool expectedResult)
     {
         Func<int, bool> isMod2 = (int i) => i % 2 == 0;
         Func<int, bool>? negateIsMode2 = isMod2.Negate();
         bool actualResult = negateIsMode2(arg);
         Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [TestMethod]
+    public void QuickSort_Success()
+    {
+        List<int> list = new() { -4, 1, 25, 50, 8, 10, 23 };
+        List<int> expectedList = new() { -4, 1, 8, 10, 23, 25, 50 };
+        List<int> sortedList = list.QuickSort();
+        CollectionAssert.AreEqual(expectedList, sortedList);
+    }
+
+    [TestMethod]
+    public void QuickSort_WithComparison_Success()
+    {
+        List<int> list = new() { -4, 1, 25, 50, 8, 10, 23 };
+        List<int> expectedList = new() { -4, 1, 8, 10, 23, 25, 50 };
+        List<int> sortedList = list.QuickSort((x, y) => x < y ? -1 : x == y ? 0 : 1);
+        CollectionAssert.AreEqual(expectedList, sortedList);
     }
 }
